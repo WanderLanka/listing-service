9583d3dfee2c3b22ed009575ecde8e5b22acc639
praneeshsuren <praneeshsuren@gmail.com>
2025-10-16 06:25:39 +0530

    fix: remove guide listing

--- src/models/User.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-// Minimal User model for listing purposes. If you share a DB, keep the same collection name.
-const userSchema = new mongoose.Schema({}, { strict: false, collection: 'users' });
-
-module.exports = mongoose.model('User', userSchema);
